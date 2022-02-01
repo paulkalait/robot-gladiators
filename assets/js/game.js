f7b5984213c5b1059ec1e5785b5c38bfe8c3f49f
Your Name <kalaitzidispaul@gmail.com>
2022-01-31 21:09:54 -0500

    added more if

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,6 +2,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 //You Can also log multiple values at once like this 
 console.log(playerName, playerHealth, playerAttack);
@@ -16,11 +17,13 @@ var fight = function() {
 
 fight(); {
     //Alert players that they are starting the round
-    
-    //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
-    enemyHealth = enemyHealth - playerAttack;
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose. ");
+
+    if (promptFight === "fight" || promptFight === "FIGHT"){
+     //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
+        enemyHealth = enemyHealth - playerAttack;
     //log a resulting message to the console so we know that it worked
-    console.log(
+     console.log(
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. "
         );
     // check enemy's health
@@ -45,6 +48,19 @@ fight(); {
     else {
         window.alert(playerName + " still has " + playerHealth + " health left.");
     }
-    
-    
-};
+    //check player's health
+ } else if (promptFight === "skip" || promptFight === "SKIP") {
+     //confirm player wants to skip
+     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+     //if yes (true), leave fight
+     if(confirmSkip){
+         window.alert(playerName + " has decided to skip this fight, Goodbye!");
+         //subtract money from playerName for skipping
+         playerMoney = playerMoney - 2;
+     }
+     //if no (false), ask question again by running fight() again
+ }  else{
+     fight();
+ }
+}
